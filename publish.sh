9e7810f227e66e02de83d66a2d8cf8a42b7291ce
Patrick Veverka <patrick.veverka@gmail.com>
2018-06-03 23:03:16 -0400

    posts for ${CURRENTEPOCTIME}

--- publish.sh
+++ publish.sh
@@ -2,5 +2,5 @@ source .env
 bundle exec ruby pocket.rb
 git add .
 CURRENTDATE=`date +"%Y-%m-%d %T"`
-git commit -m "posts for $CURRENTEPOCTIME"
+git commit -m 'posts for ${CURRENTEPOCTIME}'
 git push origin master
